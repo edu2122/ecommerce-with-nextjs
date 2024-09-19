e278490ebe5d5fdccf6bef7af21dd662b8f4f33a
Eduardo <eduardorafaellm@gmail.com>
2024-09-19 09:55:27 -0400

    Remove package-lock.json, add utils of supabase to configurate database and include next.config.mjs in .eslintrc.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { SectionFeaturedProducts } from './section-featured-products'
 import { SectionHero } from './section-hero'
 import { SectionSpecialsOffers } from './section-specials-offers'
+import { createClient } from '../utils/supabase/server'
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient()
+  const { data: products } = await supabase.from('products').select('*')
+  console.log(products)
   return (
     <div className="min-h-screen text-gray-800 font-sans">
       <main className="text-xl">
+        {/* <pre className="text-white">{JSON.stringify(products, null, 2)}</pre> */}
         <SectionHero />
-        <SectionFeaturedProducts />
+        <SectionFeaturedProducts products={products} />
         <SectionSpecialsOffers />
       </main>
     </div>
--- src/app/section-featured-products.tsx
+++ src/app/section-featured-products.tsx
@@ -1,7 +1,15 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
-export function SectionFeaturedProducts() {
+interface Product {
+  id: number
+  name: string
+  price: number
+  image_url: string
+  stock: number
+}
+
+export function SectionFeaturedProducts({ products }: { products: Product[] }) {
   return (
     <section className="py-16 px-4 bg-gray-50 dark:bg-black/50">
       <div className="container mx-auto">
@@ -9,28 +17,34 @@ export function SectionFeaturedProducts() {
           Featured Products
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {[1, 2, 3, 4].map((product) => (
-            <div key={product} className="group">
-              <div className="aspect-w-1 aspect-h-1 mb-4 rounded-lg overflow-hidden bg-gray-100 dark:bg-black/90">
-                <Image
-                  width={400}
-                  height={400}
-                  src="/images/Apple-iPhone-16-Pro-hero-geo-240909_inline.jpg.large.png"
-                  alt={`Product ${product}`}
-                  className="object-cover object-center w-full h-full group-hover:opacity-75 transition-opacity"
-                />
+          {products.map((product) => {
+            const { id, name, price, image_url: imageUrl } = product
+            return (
+              <div key={id} className="group">
+                <div className="aspect-w-1 aspect-h-1 mb-4 rounded-lg overflow-hidden bg-gray-100 dark:bg-black/90">
+                  <img
+                    width={400}
+                    height={400}
+                    src={imageUrl}
+                    alt={`Product ${name}`}
+                    className="object-cover object-center w-full h-full group-hover:opacity-75 transition-opacity"
+                  />
+                </div>
+                <h3 className="text-lg font-medium text-dark   dark:text-white">
+                  {`Minimal Product ${name}`}
+                </h3>
+                <p className="text-gray-600 dark:text-gray-400 text-lg mb-2">
+                  ${price}
+                </p>
+                <Button
+                  variant="outline"
+                  className="mt-2 w-full dark:text-white"
+                >
+                  Add to Cart
+                </Button>
               </div>
-              <h3 className="text-lg font-medium text-dark   dark:text-white">
-                Minimal Product {product}
-              </h3>
-              <p className="text-gray-600 dark:text-gray-400 text-lg mb-2">
-                $99.99
-              </p>
-              <Button variant="outline" className="mt-2 w-full dark:text-white">
-                Add to Cart
-              </Button>
-            </div>
-          ))}
+            )
+          })}
         </div>
       </div>
     </section>
